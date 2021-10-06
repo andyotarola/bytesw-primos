358adbb02b47ade22de73699e14bd40809fbf412
Andy otarola <otarolaandy@gmail.com>
2021-10-06 18:19:45 -0500

    First commit

---
+++ app.ts
@@ -0,0 +1,34 @@
+//Validar si un número es primo
+const isPrime = (num: number): boolean => {
+
+    for (let i = 2; i < num; i++) {
+  
+      if (num % i === 0) {
+        return false;
+      }
+  
+    }
+  
+    return num !== 1;
+}
+
+const firstPrimes = (total: number) => {
+
+    const primes: number[] = []
+
+    let count: number = 1
+
+    while(primes.length < total){
+
+        count++;
+
+        if(isPrime(count)){
+            primes.push(count)
+        }
+    }
+
+    return primes
+
+}
+
+console.log(firstPrimes(10));
